cd83f17c6d8deddd49b5e524214cc55d9fb77ac3
GabrielValle91 <gvalle91@gmail.com>
2018-09-25 17:17:39 -0500

    added more selectiveness on shipment detail display for assigned shipments

--- app/assets/javascripts/dispatch.js
+++ app/assets/javascripts/dispatch.js
@@ -171,19 +171,23 @@ function populateAssignedDetails(id){
     let newEle = `<h5 class='col-md-12'>Shipment ${shipmentData.id} Details <a href='/shipments/${shipmentData.id}/edit' target='_blank'>Edit</a></h5>`
     newEle += `<div class="row col-md-12"><div class="col-md-6">Reference: ${shipmentData.reference ? shipmentData.reference : ''}</div><div class="col-md-6">Status: ${shipmentData.shipment_status}</div></div>`
     newEle += `<div class="row col-md-12"><div class="col-md-6">Client: ${shipmentData.client ? shipmentData.client.name : ''}</div><div class="col-md-6">Invoice Date: ${getFormattedDate(shipmentData.invoice_date)}</div></div>`
+    shipmentData.shipment_stops.sort(function(x,y){ return x.id - y.id })
     shipmentData.shipment_stops.forEach(function(shipment_stop){
-      newEle += '<div class="col-md-12 border border-dark">'
-      newEle += `<div class="row col-md-12"><div class="col-md-3">Stop: ${shipment_stop.stop_number}</div><div class="col-md-6">Action Office: ${shipment_stop.office.name}</div></div>`
-      newEle += `<div class="row col-md-12"><div class="col-md-5">Date: ${getFormattedDate(shipment_stop.stop_start)}</div><div class="col-md-7">Times: ${shipment_stop.stop_start_time ? getFormattedTime(shipment_stop.stop_start_time) : ''} - ${shipment_stop.stop_end_time ? getFormattedTime(shipment_stop.stop_end_time) : ''}</div></div>`
-      newEle += "<br>"
-      newEle += `<div class="row col-md-12"><div class='col-md-12'>Location: ${shipment_stop.location ? shipment_stop.location.company_name : ''}</div></div>`
-      newEle += `<div class="row col-md-12"><div class='col-md-12'>${shipment_stop.location ? shipment_stop.location.address1 : ''} ${shipment_stop.location ? shipment_stop.location.address2 : ''}</div></div>`
-      newEle += `<div class="row col-md-12"><div class='col-md-12'>${shipment_stop.location ? shipment_stop.location.city : ''} ${shipment_stop.location ? shipment_stop.location.state : ''} ${shipment_stop.location ? shipment_stop.location.zip : ''}</div></div>`
-      newEle += "<br>"
-      newEle += `<div class="row col-md-12"><div class='col-md-12'>Driver: ${shipment_stop.driver ? shipment_stop.driver.name : ''}</div></div>`
-      newEle += `<div class="row col-md-12"><div class='col-md-6'>Vehicle: ${shipment_stop.vehicle ? shipment_stop.vehicle.vehicle_number : ''}</div><div class='col-md-6'>Trailer: ${shipment_stop.trailer ? shipment_stop.trailer.trailer_number : ''}</div></div>`
-      newEle += `<div class="row col-md-12"><div class='col-md-6'>Status: ${shipment_stop.stop_status}</div><div class='col-md-6'>Times: ${shipment_stop.stop_arrival_time ? getFormattedTime(shipment_stop.stop_arrival_time) : ''} - ${shipment_stop.stop_departure_time ? getFormattedTime(shipment_stop.stop_departure_time) : ''}</div></div>`
-      newEle += '</div>'
+      let currentDriver = $(".selected-driver")
+      if((currentDriver.length && shipment_stop.driver && (shipment_stop.driver.name == currentDriver[0].innerHTML)) || (!currentDriver.length && shipment_stop.driver && shipment_stop.driver.id)){
+        newEle += '<div class="col-md-12 border border-dark">'
+        newEle += `<div class="row col-md-12"><div class="col-md-3">Stop: ${shipment_stop.stop_number}</div><div class="col-md-6">Action Office: ${shipment_stop.office.name}</div></div>`
+        newEle += `<div class="row col-md-12"><div class="col-md-5">Date: ${getFormattedDate(shipment_stop.stop_start)}</div><div class="col-md-7">Times: ${shipment_stop.stop_start_time ? getFormattedTime(shipment_stop.stop_start_time) : ''} - ${shipment_stop.stop_end_time ? getFormattedTime(shipment_stop.stop_end_time) : ''}</div></div>`
+        newEle += "<br>"
+        newEle += `<div class="row col-md-12"><div class='col-md-12'>Location: ${shipment_stop.location ? shipment_stop.location.company_name : ''}</div></div>`
+        newEle += `<div class="row col-md-12"><div class='col-md-12'>${shipment_stop.location ? shipment_stop.location.address1 : ''} ${shipment_stop.location ? shipment_stop.location.address2 : ''}</div></div>`
+        newEle += `<div class="row col-md-12"><div class='col-md-12'>${shipment_stop.location ? shipment_stop.location.city : ''} ${shipment_stop.location ? shipment_stop.location.state : ''} ${shipment_stop.location ? shipment_stop.location.zip : ''}</div></div>`
+        newEle += "<br>"
+        newEle += `<div class="row col-md-12"><div class='col-md-12'>Driver: ${shipment_stop.driver ? shipment_stop.driver.name : ''}</div></div>`
+        newEle += `<div class="row col-md-12"><div class='col-md-6'>Vehicle: ${shipment_stop.vehicle ? shipment_stop.vehicle.vehicle_number : ''}</div><div class='col-md-6'>Trailer: ${shipment_stop.trailer ? shipment_stop.trailer.trailer_number : ''}</div></div>`
+        newEle += `<div class="row col-md-12"><div class='col-md-6'>Status: ${shipment_stop.stop_status}</div><div class='col-md-6'>Times: ${shipment_stop.stop_arrival_time ? getFormattedTime(shipment_stop.stop_arrival_time) : ''} - ${shipment_stop.stop_departure_time ? getFormattedTime(shipment_stop.stop_departure_time) : ''}</div></div>`
+        newEle += '</div>'
+      }
     })
     $("#assigned-shipment-details").append(newEle);
   });
--- app/assets/javascripts/shipments.js
+++ app/assets/javascripts/shipments.js
@@ -646,6 +646,7 @@ newStopButton = () => {
 submitForm = () => {
   $("#commit_changes").on('click', function(e){
     e.preventDefault();
+    window.location.href = '/shipments'
   })
 }
 
